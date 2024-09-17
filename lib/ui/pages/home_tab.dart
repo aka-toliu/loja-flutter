08c561a75ec5861b7b3a052f90d8a2f37616dc02
Tulio Santos <tulio.kprod@gmail.com>
2024-09-17 18:31:52 -0300

    images grid home

--- lib/ui/pages/home_tab.dart
+++ lib/ui/pages/home_tab.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
@@ -20,9 +23,9 @@ class HomeTab extends StatelessWidget {
     return Stack(
       children: [
         _buildBodyBack(),
-        const CustomScrollView(
+        CustomScrollView(
           slivers: [
-            SliverAppBar(
+            const SliverAppBar(
               floating: true,
               snap: true,
               backgroundColor: Colors.transparent,
@@ -31,7 +34,39 @@ class HomeTab extends StatelessWidget {
                   title: Text('Home', style: TextStyle(color: Colors.white),),
                 centerTitle: true,
               ),
-            )
+            ),
+            FutureBuilder<QuerySnapshot>(
+                future: FirebaseFirestore.instance.collection('home').orderBy('pos').get(),
+                builder: (context, snapshot){
+                  if(!snapshot.hasData){
+                    return SliverToBoxAdapter(
+                      child: Container(
+                        height: 200,
+                        alignment: Alignment.center,
+                        child: const CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(Colors.white),
+                        ),
+                      ),
+                    );
+                  }else{
+                    return SliverToBoxAdapter(
+                      child: StaggeredGrid.count(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 1,
+                        crossAxisSpacing: 1,
+                        children: snapshot.data!.docs.map((doc) => StaggeredGridTile.count(
+                          crossAxisCellCount: doc["x"],
+                          mainAxisCellCount: doc["y"],
+                          child: FadeInImage.memoryNetwork(
+                              placeholder: kTransparentImage,
+                              image: doc["image"],
+                              fit: BoxFit.cover,
+                          ),
+                        )).toList(),
+                      ),
+                    );
+                  }
+                })
           ],
         )
       ],
