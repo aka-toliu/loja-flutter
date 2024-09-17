f5dc5e01e366f94fafc305878cd22fc954c99d56
Tulio Santos <tulio.kprod@gmail.com>
2024-09-17 11:43:39 -0300

    initial commit

---
+++ android/app/src/main/kotlin/br/com/toliu/loja/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.toliu.loja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:loja/ui/pages/login_page.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: LoginPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
