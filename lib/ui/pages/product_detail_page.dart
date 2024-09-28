2e754fb512d16d93dcca844e122f61ba2057ea1a
Tulio Santos <tulio.kprod@gmail.com>
2024-09-28 11:48:54 -0300

    style: finishing product detail page

--- lib/ui/pages/product_detail_page.dart
+++ lib/ui/pages/product_detail_page.dart
@@ -17,6 +17,8 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
 
   final ProductData? data;
 
+  String? size;
+
   _ProductDetailPageState(this.data);
 
   @override
@@ -53,9 +55,9 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                 Text(product["title"], style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 22), maxLines: 3,),
                 Text("R\$ ${product["price"].toStringAsFixed(2)} ", style: const TextStyle(fontSize: 24),),
                 const SizedBox(height: 16,),
-                const Text("Tamanho", style: TextStyle(fontSize: 16),),
+                const Text("Tamanho", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
                 SizedBox(
-                  height: 34,
+                  height: 42,
                   child: GridView(
                     padding: const EdgeInsets.symmetric(vertical: 8),
                     scrollDirection: Axis.horizontal,
@@ -67,21 +69,40 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                     children: data!.sizes!.map(
                         (s){
                           return GestureDetector(
+                            onTap: (){
+                              setState(() {
+                                size = s;
+                              });
+                            },
                               child: Container(
                                 decoration: BoxDecoration(
                                   borderRadius: const BorderRadius.all(Radius.circular(4)),
                                   border: Border.all(
-                                    color: Colors.grey
+                                    color: size == s ?  Colors.blue :  Colors.grey,
+                                    width: 3
                                   )
                                 ),
                                 width: 50,
                                 alignment: Alignment.center,
-                                child: s is String ? Text(s) : Text('Unknown Size'),
+                                child: s is String ? Text(s) : Text('Unknown Size', style: TextStyle(fontSize: 24),),
                               ),
                           );
                         }
                     ).toList(),
                   ),
+                ),
+                const SizedBox(height: 16,),
+                SizedBox(height: 54,
+                child: ElevatedButton(
+                  onPressed: size != null ? (){} : null,
+                  style: ElevatedButton.styleFrom(backgroundColor: primaryColor),
+                  child: const Text('Adicionar ao carrinho', style: TextStyle(fontSize: 18, color: Colors.white),),
+                ),),
+                const SizedBox(height: 16,),
+                const Text("Descrição", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                Text(
+                    product["description"],
+                  style: const TextStyle(fontSize: 16),
                 )
               ],
             ),
